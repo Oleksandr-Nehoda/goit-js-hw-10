7f6e21bcc51916c18a581d8b44b2149ef20baa2c
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-10-07 10:46:55 +0300

    add spiner

--- src/index.js
+++ src/index.js
@@ -2,6 +2,42 @@ import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import './css/styles.css';
 import API from './fetchCountries';
+// ______________________________________________________________Spiner
+
+import {Spinner} from 'spin.js';
+import "spin.js/spin.css";
+
+var opts = {
+    lines: 10, // The number of lines to draw
+  length: 20, // The length of each line
+  width: 6, // The line thickness
+  radius: 9, // The radius of the inner circle
+    scale: 1, // Scales overall size of the spinner
+    corners: 1, // Corner roundness (0..1)
+    speed: 1, // Rounds per second
+    rotate: 0, // The rotation offset
+    animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000000', // CSS color or array of colors
+    fadeColor: 'transparent', // CSS color or array of colors
+    top: '90%', // Top position relative to parent
+    left: '50%', // Left position relative to parent
+    shadow: '0 0 1px transparent', // Box-shadow for the lines
+    zIndex: 2000000000, // The z-index (defaults to 2e9)
+    className: 'spinner', // The CSS class to assign to the spinner
+    position: 'relativ', // Element positioning
+  };
+  
+//   var target = document.getElementById('foo');
+//   var spinner = new Spinner(opts).spin();
+
+
+// setTimeout (() => {
+//     spinner.stop();
+// },1000); 
+  
+
+// ______________________________________________________________
 
 const DEBOUNCE_DELAY = 300;
 
@@ -11,6 +47,13 @@ const refs = {
     countryInfo: document.querySelector('.country-info'),
 }
 
+// var spinner = new Spinner(opts).spin(refs.countryList);
+
+
+// setTimeout (() => {
+//     spinner.stop();
+// },1000); 
+  
 refs.input.addEventListener('input', debounce(onRenderCountry, DEBOUNCE_DELAY));
 
 function onRenderCountry (event) {
@@ -18,8 +61,11 @@ function onRenderCountry (event) {
     const inputValue = (event.target.value).trim();
 
     if (inputValue.length > 0) {
+
+        spinner = new Spinner(opts).spin(refs.countryList);
        return API.fetchCountries(inputValue).then(renderCountry)
        .catch(() => {
+        // spinner.stop();
         notificationFeil()
         deleteCountryInfo();
         deleteCountryList()
@@ -29,10 +75,12 @@ function onRenderCountry (event) {
 
 function renderCountry (array) {
     if (array.length > 10){
+        // spinner.stop();
         notificationInfo();
         deleteCountryInfo();
         deleteCountryList();
     } else if(array.length > 1){
+        // spinner.stop();
         deleteCountryInfo();
         deleteCountryList();
     array.map(({name, flags}) => {
@@ -41,6 +89,7 @@ function renderCountry (array) {
       refs.countryList.insertAdjacentHTML('beforeend', makeMarkupCountryList(nameCountry, flag));
     })
     } else if(array.length === 1){
+        // spinner.stop();
         deleteCountryList();
         const object = array[0];
         const {name, capital, population, flags, languages} = object;
@@ -85,3 +134,7 @@ function makeMarkupCountryInfo (nameCountry, flag, capitalCountry, populationCou
     <p class="text-info"><span class="text-info--blod">Population: </span>${populationCountry}</p>
     <p class="text-info"><span class="text-info--blod">Languages: </span>${language}</p>`; 
 }
+
+
+// Тренувальний код____________________________________________________
+
